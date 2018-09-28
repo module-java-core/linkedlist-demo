94c7919c7f7502733811354e7016fea997a076ed
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-28 11:55:36 +0700

    create application to run program

---
+++ src/Application.java
@@ -0,0 +1,13 @@
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        LinkedList ll = new LinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.printList();
+    }
+}
